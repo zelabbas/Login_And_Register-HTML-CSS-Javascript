02a36a3d5f4d0b9cb2e339e03f521d910b5d8c74
zelabbas <zakariaelabbassi2002@gmail.com>
2024-11-22 21:38:23 +0100

    link the element each other

--- script.js
+++ script.js
@@ -3,10 +3,13 @@
 const wrapper = document.querySelector(".wrapper");
 const loginLink = document.querySelector(".login-link");
 const registerLink = document.querySelector(".register-link");
+const btnPopup = document.querySelector(".btnLogin-popup");
+const closePopup = document.querySelector(".icon-close");
 
 console.log(wrapper);
 console.log(loginLink);
 console.log(registerLink);
+console.log(btnPopup);
 
 registerLink.addEventListener("click", () => {
   wrapper.classList.add("active");
@@ -14,4 +17,12 @@ registerLink.addEventListener("click", () => {
 
 loginLink.addEventListener("click", () => {
   wrapper.classList.remove("active");
+});
+
+btnPopup.addEventListener("click", () => {
+  wrapper.classList.add("active-login");
+});
+
+closePopup.addEventListener("click", () => {
+  wrapper.classList.remove("active-login");
 });
