cc6fef487dcd63bce33570e19b06a0f6d81983ea
zelabbas <zakariaelabbassi2002@gmail.com>
2024-11-22 15:28:21 +0100

    link the registration with the login

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 "use strict";
+
+const wrapper = document.querySelector(".wrapper");
+const loginLink = document.querySelector(".login-link");
+const registerLink = document.querySelector(".register-link");
+
+console.log(wrapper);
+console.log(loginLink);
+console.log(registerLink);
+
+registerLink.addEventListener("click", () => {
+  wrapper.classList.add("active");
+});
+
+loginLink.addEventListener("click", () => {
+  wrapper.classList.remove("active");
+});
